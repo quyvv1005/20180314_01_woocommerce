503b1c923f2309a6a933a91dd04a26c907e6b7b5
quyvv <quyvv1005@gmail.com>
2018-03-29 13:11:31 +0700

    updated

---
+++ wp-content/themes/flatsome/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package flatsome
+ */
+
+global $flatsome_opt;
+?>
+
+</main><!-- #main -->
+
+<footer id="footer" class="footer-wrapper">
+
+	<?php do_action('flatsome_footer'); ?>
+
+</footer><!-- .footer-wrapper -->
+
+</div><!-- #wrapper -->
+
+<?php wp_footer(); ?>
+
+<!-- <script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyBzqpB7QpDl2ogWIor6MfGFbm67i2CzBHA"></script> -->
+</body>
+</html>
